99fffc26a91b14517d443ffe9d0b5c6f8c35d003
Tulio Jeunon <tuliojeunondev@gmail.com>
2024-03-04 17:14:23 -0300

    Incluindo Swagger OpenApi

--- src/main/java/br/com/insuranceapi/InsuranceApiApplication.java
+++ src/main/java/br/com/insuranceapi/InsuranceApiApplication.java
@@ -1,9 +1,12 @@
 package br.com.insuranceapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "Itau Challenge"))
 public class InsuranceApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/insuranceapi/controller/CustomerInsuranceController.java
+++ src/main/java/br/com/insuranceapi/controller/CustomerInsuranceController.java
@@ -3,6 +3,9 @@ package br.com.insuranceapi.controller;
 import br.com.insuranceapi.dto.CustomerDTO;
 import br.com.insuranceapi.dto.CustomerInsuranceDTO;
 import br.com.insuranceapi.service.CustomerInsuranceService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +17,19 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/v1/insurance")
+@Tag(name = "insurances")
 public class CustomerInsuranceController {
 
     @Autowired
     private CustomerInsuranceService customerInsuranceService;
 
     @PostMapping("/find")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "Invalid Data"),
+            @ApiResponse(responseCode = "422", description = "Invalid Location"),
+            @ApiResponse(responseCode = "500", description = "Integration failed")
+    })
     public ResponseEntity<CustomerInsuranceDTO> findInsurancesByCustomer(@RequestBody @Valid CustomerDTO customerDTO){
         return ResponseEntity.ok().body(customerInsuranceService.findInsuranceByCustomer(customerDTO));
     }
